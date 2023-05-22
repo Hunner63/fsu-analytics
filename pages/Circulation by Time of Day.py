b5401a26710f8ae4e2937ff14dad933d8d280b05
Hunner63 <eric.dodson@gmail.com>
2023-05-22 12:54:58 -0500

    extra ,

--- pages/Circulation by Time of Day.py
+++ pages/Circulation by Time of Day.py
@@ -84,13 +84,13 @@ else:
 
         # Set the y-axis label
         ax.set_ylabel('Counts', fontsize=12, fontweight="bold")
-        ax.set_xlabel('Hours', , fontsize=12, fontweight="bold")
+        ax.set_xlabel('Hours', fontsize=12, fontweight="bold")
 
      #   date_formatter = mdates.DateFormatter('%I:00 %p')  # Format as "HH:00 AM/PM"
      #   ax.xaxis.set_major_formatter(date_formatter)
 
         # Set the title and legend
-        ax.set_title('Allocation Counts by Hour of the Day' , fontsize=18, fontweight="bold")
+        ax.set_title('Allocation Counts by Hour of the Day', fontsize=18, fontweight="bold")
         ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
         # Display the combo bar graph
