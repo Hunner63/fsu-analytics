f9d31fb0c6ef5264aae0a89d131c4eeda8828c7d
Hunner63 <eric.dodson@gmail.com>
2023-05-31 16:22:28 -0500

    Styling changes and misc changes based on Fanshawe feedback

--- pages/Resource_Usage_by_Date_Range.py
+++ pages/Resource_Usage_by_Date_Range.py
@@ -25,11 +25,32 @@ unique_rtype = data.sort_values('rtype')['rtype']
 selected_rtype = st.sidebar.selectbox("Select an rtype", unique_rtypes)
 
 # Filter the data based on the date range and rtype
-filtered_data = data[(data['startdatetime'] >= start_date) & (data['startdatetime'] <= end_date) & (data['rtype'] == selected_rtype)]
+filtered_type_data = data[(data['startdatetime'] >= start_date) & (data['startdatetime'] <= end_date) & (data['rtype'] == selected_rtype)]
+filtered_data = data[(data['startdatetime'] >= start_date) & (data['startdatetime'] <= end_date)]
+grouped_data = filtered_data.groupby('rtype').size().reset_index(name='Turndowns')
 
 # Calculate the count of occurances
-count = filtered_data.shape[0]
+count = filtered_type_data.shape[0]
+st.markdown(f"<h1 style='text-align: center; color: black;'>Occurances of selected Resource Type by Date Range</h1>", unsafe_allow_html=True)
+#st.table(filtered_type_data[['ckid', 'startdatetime']])
+#st.write("Usage for:", selected_rtype)
+st.markdown(f"**Usage for:** {selected_rtype}")
 
-st.markdown(f"<h1 style='text-align: center; color: black;'>The number of occurances of the selected rtype within the selected date range is: {count}</h1>", unsafe_allow_html=True)
-
-st.table(filtered_data[['ckid', 'startdatetime']])
+hide_table_row_index = """
+            <style>
+            table {width: 50% !important}
+            thead tr th:first-child {display:none}
+            thead th {font-weight: bold; color: blue; background-color: rgba(0, 0, 0, 0.10)}
+            tbody th {display:none}
+            </style>
+            """
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+grouped_data = grouped_data.rename(columns={'rtype': 'Resource Type'})
+st.table(grouped_data[grouped_data["Resource Type"] == selected_rtype])
+st.markdown(f"**Most used items:** {selected_rtype}")
+sorted_data = grouped_data[['Resource Type', 'Turndowns']].nlargest(10, 'Turndowns').sort_values(by='Turndowns', ascending=False)
+st.table(sorted_data)
+st.markdown(f"**ULeast used items:** {selected_rtype}")
+#st.write(sorted_data['rtype'], sorted_data['counts'])
+sorted_data = grouped_data[['Resource Type', 'Turndowns']].nsmallest(10, 'Turndowns').sort_values(by='Turndowns', ascending=True)
+st.table(sorted_data)
--- pages/Turndowns.py
+++ pages/Turndowns.py
@@ -19,11 +19,26 @@ unique_rtypes = turndown_data.sort_values('rtype')['rtype']
 selected_rtype = st.sidebar.selectbox("Select an rtype", unique_rtypes)
 
 # Filter the data based on the date range and rtype
-filtered_data = turndown_data[(turndown_data['turndowndatetime'] >= start_date) & (turndown_data['turndowndatetime'] <= end_date) & (turndown_data['rtype'] == selected_rtype)]
+filtered_rtype_data = turndown_data[(turndown_data['turndowndatetime'] >= start_date) & (turndown_data['turndowndatetime'] <= end_date) & (turndown_data['rtype'] == selected_rtype)]
+filtered_data = turndown_data[(turndown_data['turndowndatetime'] >= start_date) & (turndown_data['turndowndatetime'] <= end_date)]
+grouped_selected_data = filtered_rtype_data.groupby("rtype").size().reset_index(name='Counts')
+grouped_data = filtered_data.groupby("rtype").size().reset_index(name='Counts')
 
 # Calculate the count of occurances
 count = filtered_data.shape[0]
 
-st.markdown(f"<h1 style='text-align: center; color: black;'>Turndowns of selected rtype within  selected date range: {count}</h1>", unsafe_allow_html=True)
+st.markdown(f"<h1 style='text-align: center; color: black;'>Turndowns of selected rtype within date range: </h1>", unsafe_allow_html=True)
+hide_table_row_index = """
+            <style>
+            table {width: 50% !important}
+            thead tr th:first-child {display:none}
+            thead th {font-weight: bold; color: blue; background-color: rgba(0, 0, 0, 0.10)}
+            tbody th {display:none}
+            </style>
+            """
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+grouped_data = grouped_data.rename(columns={'rtype': 'Resource Type'})
+grouped_selected_data = grouped_data.rename(columns={'rtype': 'Resource Type'})
+st.table(grouped_data[grouped_data['Resource Type'] == selected_rtype])
+st.table(grouped_data.nlargest(10, 'Counts').sort_values(by='Counts', ascending=False))
 
-st.table(filtered_data[['rtype', 'turndowndatetime']])
