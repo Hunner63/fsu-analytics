478ad7b059c8694b2ddfb595a2eda42b8f6208bc
Hunner63 <eric.dodson@gmail.com>
2023-06-03 14:11:52 -0500

    Changes to Resource USage by Date Range

--- pages/Resource_Usage_by_Date_Range.py
+++ pages/Resource_Usage_by_Date_Range.py
@@ -3,38 +3,38 @@ st.set_page_config(layout="wide")
 import pandas as pd
 
 # Read in the data
-data = pd.read_csv("AprilMayAllocsTurndowns.csv", names=['ckid', 'resource', 'rtype', 'status', 'startdatetime', 'enddatetime', 'actualdatetime'])
+df = pd.read_csv("AprilMayAllocsTurndowns.csv", names=['ckid', 'resource', 'rtype', 'status', 'startdatetime', 'enddatetime', 'actualdatetime'])
 
 #convert to dates
-data['startdatetime'] = pd.to_datetime(data['startdatetime'])
-data['startdatetime'] = data['startdatetime'].dt.date
-data['enddatetime'] = pd.to_datetime(data['enddatetime'])
-data['enddatetime'] = data['enddatetime'].dt.date
-data['actualdatetime'] = pd.to_datetime(data['actualdatetime'])
-data['actualdatetime'] = data['actualdatetime'].dt.date
+df['startdatetime'] = pd.to_datetime(df['startdatetime']).dt.date
+#df['startdatetime'] = data['startdatetime'].dt.date
+df['enddatetime'] = pd.to_datetime(df['enddatetime']).dt.date
+#df['enddatetime'] = data['enddatetime'].dt.date
+df['actualdatetime'] = pd.to_datetime(df['actualdatetime']).dt.date
+#df['actualdatetime'] = data['actualdatetime'].dt.date
 
 # Create a date range selector
-start_date = st.sidebar.date_input("Start Date")
-end_date = st.sidebar.date_input("End Date")
+startDate = st.sidebar.date_input("Start Date")
+endDate = st.sidebar.date_input("End Date")
 
 # Create a list of unique rtypes
-unique_rtypes = data['rtype'].unique()
-unique_rtype = data.sort_values('rtype')['rtype']
+uniqueRtypes = df['rtype'].unique()
+uniqueRtype = df.sort_values('rtype')['rtype']
 
 # Select the rtype
-selected_rtype = st.sidebar.selectbox("Select an rtype", unique_rtypes)
+selectedRtype = st.sidebar.selectbox("Select an rtype", uniqueRtypes)
 
 # Filter the data based on the date range and rtype
-filtered_type_data = data[(data['startdatetime'] >= start_date) & (data['startdatetime'] <= end_date) & (data['rtype'] == selected_rtype)]
-filtered_data = data[(data['startdatetime'] >= start_date) & (data['startdatetime'] <= end_date)]
-grouped_data = filtered_data.groupby('rtype').size().reset_index(name='Turndowns')
+#filtered_type_data = data[(data['startdatetime'] >= start_date) & (data['startdatetime'] <= end_date) & (data['rtype'] == selected_rtype)]
+filteredData = df[(df['startdatetime'] >= startDate) & (df['startdatetime'] <= endDate)]
+groupedData = filteredData.groupby('rtype').size().reset_index(name='Count')
 
 # Calculate the count of occurances
-count = filtered_type_data.shape[0]
+count = filteredData.shape[0]
 st.markdown(f"<h1 style='text-align: center; color: black;'>Occurances of selected Resource Type by Date Range</h1>", unsafe_allow_html=True)
 #st.table(filtered_type_data[['ckid', 'startdatetime']])
 #st.write("Usage for:", selected_rtype)
-st.markdown(f"**Usage for:** {selected_rtype}")
+st.markdown(f"**Usage for:** {selectedRtype}")
 
 hide_table_row_index = """
             <style>
@@ -45,12 +45,12 @@ hide_table_row_index = """
             </style>
             """
 st.markdown(hide_table_row_index, unsafe_allow_html=True)
-grouped_data = grouped_data.rename(columns={'rtype': 'Resource Type'})
-st.table(grouped_data[grouped_data["Resource Type"] == selected_rtype])
-st.markdown(f"**Most used items:** ")
-sorted_data = grouped_data[['Resource Type', 'Turndowns']].nlargest(10, 'Turndowns').sort_values(by='Turndowns', ascending=False)
-st.table(sorted_data)
-st.markdown(f"**Least used items:**")
-#st.write(sorted_data['rtype'], sorted_data['counts'])
-sorted_data = grouped_data[['Resource Type', 'Turndowns']].nsmallest(10, 'Turndowns').sort_values(by='Turndowns', ascending=True)
+groupedData = groupedData.rename(columns={'rtype': 'Resource Type'})
+st.table(groupedData[groupedData["Resource Type"] == selectedRtype])
+st.markdown(f"**Most frequently used :** ")
+sortedData = groupedData[['Resource Type', 'Count']].nlargest(10, 'Count').sort_values(by='Count', ascending=False)
+st.table(sortedData)
+st.markdown(f"**Least frequenctly used:**")
+st.write(f"The total number of resource types with only one checkout is {len(groupedData[groupedData['Count'] == 1])}")
+sorted_data = groupedData[['Resource Type', 'Count']].nsmallest(10, 'Count').sort_values(by='Count', ascending=True)
 st.table(sorted_data)
